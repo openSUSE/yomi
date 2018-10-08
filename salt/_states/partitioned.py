d167d0921e6e8a466e46aae3e5c57057a69c19f8
Alberto Planas <aplanas@gmail.com>
2018-10-08 15:55:11 +0200

    Add the mkparted and named states

--- salt/_states/partitioned.py
+++ salt/_states/partitioned.py
@@ -23,6 +23,7 @@
 '''
 from __future__ import absolute_import, print_function, unicode_literals
 import logging
+import re
 
 import salt.utils.path
 
@@ -46,6 +47,10 @@ except NameError:
     __pillars__ = {}
     __salt__ = {}
     __states__ = {}
+
+
+class EnumerateException(Exception):
+    pass
 
 
 def __virtual__():
@@ -217,4 +222,348 @@ def devices(name, udev_in=None, udev_ex=None):
         'all_devices': all_devices,
         'devices': sorted(devices_udev_key_in-devices_udev_key_ex),
     }
+    return ret
+
+
+def _get_partition_type(device):
+    '''
+    Get partition type of each partition
+
+    Return a list of tuples: [(number, type), ...]
+
+    '''
+    cmd = 'parted -s {0} print'.format(device)
+    out = __salt__['cmd.run_stdout'](cmd)
+    return re.findall(r'\s*(\d+).*(primary|extended|logical).*', out)
+
+
+def _get_cached_info(device):
+    '''
+    Get the information of a device as a dictionary
+
+    '''
+    if not hasattr(_get_cached_info, 'info'):
+        _get_cached_info.info = __salt__['partition.list'](device)['info']
+    return _get_cached_partitions.info
+
+
+def _get_cached_partitions(device, unit='s'):
+    '''
+    Get the partitions as a dictionary
+
+    '''
+    # `partitions` will be used as a local cache, to avoid multiple
+    # request of the same partition with the same units. Is a
+    # dictionary where the key is the `unit`, as we will make request
+    # of all partitions under this unit. This potentially can low the
+    # complexity algorithm to amortized O(1).
+    if not hasattr(_get_cached_partitions, 'partitions'):
+        _get_cached_partitions.partitions = {}
+        # There is a bug in `partition.list`, where `type` is storing
+        # the file system information, to workaround this we get the
+        # partition type using parted and attach it here.
+        _get_cached_partitions.types = _get_partition_type(device)
+    partitions = _get_cached_partitions.partitions
+
+    if unit not in partitions:
+        partitions[unit] = __salt__['partition.list'](device, unit=unit)
+        # We manually add the types in the partition information
+        for number, type_ in _get_cached_partitions.types:
+            partitions[unit]['partitions'][number]['type'] = type_
+
+    return partitions[unit]['partitions']
+
+
+def _parse_value_with_units(value, default='MB'):
+    '''
+    Split a value with (optionally) is expressed in units.
+
+    Returns the tuple (value, unit)
+    '''
+    valid_units = ('s', 'B', 'kB', 'MB', 'MiB', 'GB', 'GiB', 'TB',
+                   'TiB', '%', 'cyl', 'chs', 'compact')
+    match = re.search(r'^([\d.]+)(\D*)$', str(value))
+    if match:
+        value, unit = match.groups()
+        unit = unit if unit else default
+        if unit in valid_units:
+            return (str(value), unit)
+
+
+def _check_partition(device, number, part_type, start, end):
+    '''
+    Check if the proposed partition match the current one.
+
+    Returns a tri-state value:
+      - `True`: the proposed partition match
+      - `False`: the proposed partition do not match
+      - `None`: the proposed partition is a new partition
+    '''
+    # The `start` and `end` fields are expressed with units (the same
+    # kind of units that `parted` allows). To make a fair comparison
+    # we need to normalize each field to the same units that we can
+    # use to read the current partitions. A good candidate is sector
+    # ('s'). The problem is that we need to reimplement the same
+    # conversion logic from `parted` here [1], as we need the same
+    # round logic when we covert from 'MiB' to 's', for example.
+    #
+    # To avoid this duplicity of code we can do a trick: for each
+    # field in the proposed partition we request a `partition.list`
+    # with the same unit. We make `parted` to make the conversion for
+    # us, in exchange for an slower algorithm.
+    #
+    # We can change it once we decide to take care of alignment.
+    #
+    # [1] Check libparted/unit.c
+
+    number = str(number)
+    partitions = _get_cached_partitions(device)
+    if number not in partitions:
+        return None
+
+    if part_type != partitions[number]['type']:
+        return False
+
+    for value, name in ((start, 'start'), (end, 'end')):
+        value, unit = _parse_value_with_units(value)
+        current_value = _get_cached_partitions(device, unit)[number][name]
+        current_value, _ = _parse_value_with_units(current_value)
+        if value != current_value:
+            return False
+
+    return True
+
+
+def _get_first_overlapping_partition(device, start):
+    '''
+    Return the partition that contains the start point.
+
+    '''
+    # Check if there is a partition in the system that start at
+    # specified point.
+    value, unit = _parse_value_with_units(start)
+    value = int(value)
+    partitions = _get_cached_partitions(device, unit)
+    partition_number = None
+    partition_start = 0
+    for number, partition in partitions.items():
+        p_start = int(_parse_value_with_units(partition['start'])[0])
+        p_end = int(_parse_value_with_units(partition['end'])[0])
+        if p_start <= value and p_end >= value:
+            if not partition_number or partition_start >= value:
+                partition_number = number
+                partition_start = value
+    return partition_number
+
+
+def _get_partition_number(device, part_type, start, end):
+    '''
+    Return a partition number for a [start, end] range and a partition
+    type.
+
+    If the range is allocated and the partition type match, return the
+    partition number. If the type do not match but is a logical
+    partition inside an extended one, return the next partition
+    number.
+
+    If the range is not allocated, return the next partition number.
+
+    '''
+
+    unit = _parse_value_with_units(start)[1]
+    partitions = _get_cached_partitions(device, unit)
+
+    # Check if there is a partition in the system that start at
+    # specified point and match the same type.
+    number = _get_first_overlapping_partition(device, start)
+    if number:
+        if partitions[number]['type'] == part_type:
+            return number
+        elif not (partitions[number]['type'] == 'extended'
+                  and part_type == 'logical'):
+            raise EnumerateException('Do not overlap partitions')
+
+    def __primary_partition_free_slot(partitions, label):
+        if label == 'msdos':
+            max_primary = 4
+        else:
+            max_primary = 1024
+        for i in range(1, max_primary + 1):
+            i = str(i)
+            if i not in partitions:
+                return i
+
+    # The partition is not already there, we guess the next number
+    label = _get_cached_info(device)['partition table']
+    if part_type == 'primary':
+        candidate = __primary_partition_free_slot(partitions, label)
+        if not candidate:
+            raise EnumerateException('No free slot for primary partition')
+        return candidate
+    elif part_type == 'extended':
+        if label == 'gpt':
+            raise EnumerateException('Extended partitions not allowed in gpt')
+        if 'extended' in (info['type'] for info in partitions.values()):
+            raise EnumerateException('Already found a extended partition')
+        candidate = __primary_partition_free_slot(partitions, label)
+        if not candidate:
+            raise EnumerateException('No free slot for extended partition')
+        return candidate
+    elif part_type == 'logical':
+        if label == 'gpt':
+            raise EnumerateException('Extended partitions not allowed in gpt')
+        if 'extended' not in (part['type'] for part in partitions.values()):
+            raise EnumerateException('Missing extended partition')
+        candidate = max((int(part['number'])
+                         for part in partitions.values()
+                         if part['type'] == 'logical'), default=4)
+        return str(candidate + 1)
+
+
+def mkparted(name, part_type, fs_type=None, start=None, end=None):
+    '''
+    Make sure that a partition is allocated in the disk.
+
+    name
+        Device or partition name. If the name is like /dev/sda, parted
+        will take care of creating the partition on the next slot. If
+        the name is like /dev/sda1, we will consider partition 1 as a
+        reference for the match.
+
+    part_type
+        Type of partition, should be one of "primary", "logical", or
+        "extended".
+
+    fs_type
+        Expected filesystem, following the parted names.
+
+    start
+        Start of the partition (in parted units)
+
+    end
+        End of the partition (in parted units)
+
+    '''
+    ret = {
+        'name': name,
+        'result': False,
+        'changes': {},
+        'comment': [],
+    }
+
+    if part_type not in ('primary', 'extended', 'logical'):
+        ret['comment'].append('Partition type not recognized')
+    if not start or not end:
+        ret['comment'].append('Parameters start and end are not optional')
+
+    # If the user do not provide any partition number we get generate
+    # the next available for the partition type
+    device, number = re.search(r'(\D+)(\d*)', name).groups()
+    if not number:
+        try:
+            number = _get_partition_number(device, part_type, start, end)
+        except EnumerateException as e:
+            ret['comment'].appent(str(e))
+
+    # If at this point we have some comments, we return with a fail
+    if ret['comment']:
+        return ret
+
+    # Check if the partition is already there or we need to create a
+    # new one
+    partition_match = _check_partition(name, device, number,
+                                       part_type, fs_type, start, end)
+
+    if partition_match:
+        ret['comment'].append('Partition {}{} already '
+                              'in place'.format(device, number))
+    elif partition_match is None:
+        ret['changes']['new'] = 'Partition {}{} will ' \
+            'be created'.format(device, number)
+    elif partition_match is False:
+        ret['comment'].append('Partition {}{} cannot '
+                              'be replaced'.format(device, number))
+
+    if __opts__['test']:
+        ret['result'] = None
+        return ret
+
+    if partition_match is False:
+        # TODO(aplanas) with parted we cannot force a partition number
+        res = __salt__['partition.mkpart'](device, part_type, fs_type,
+                                           start, end)
+        ret['changed']['output'] = res
+
+    partition_match = _check_partition(name, device, number,
+                                       part_type, fs_type, start, end)
+    if partition_match:
+        ret['result'] = True
+    elif not partition_match:
+        ret['commend'].append('Partition {}{} fail to '
+                              'be created'.format(device, number))
+        ret['result'] = False
+
+    return ret
+
+
+def _check_partition_name(device, partition, name):
+    '''
+    Check if the partition have this name.
+    '''
+    pass
+
+
+def named(name, device, partition=None):
+    '''
+    Make sure that a gpt partition have set a name.
+
+    name
+        Name or label for the partition
+
+    device
+        Device name (/dev/sda, /dev/disk/by-id/scsi-...) or partition
+
+    partition
+        Partition number (can be in the device)
+
+    '''
+    ret = {
+        'name': name,
+        'result': False,
+        'changes': {},
+        'comment': [],
+    }
+
+    if not partition:
+        device, partition = re.search(r'(\D+)(\d*)', name).groups()
+    if not partition:
+        ret['comment'].append('Partition number not provided')
+
+    if not _check_label(device, 'gpt'):
+        ret['comment'].append('Only gpt partitions can be named')
+
+    if _check_partition_name(device, partition, name):
+        ret['result'] = True
+        ret['comment'].append('Name of the partition {}{} is '
+                              'already "{}"'.format(device, partition, name))
+
+    if ret['comment']:
+        return ret
+
+    if __opts__['test']:
+        ret['comment'].append('Partition {}{} will be named '
+                              '"{}"'.format(device, partition, name))
+        ret['changes']['name'] = 'Name will be set to {}'.format(name)
+        return ret
+
+    changes = __salt__['partition.name'](device, partition, name)
+
+    if _check_partition_name(device, partition, name):
+        ret['result'] = True
+        ret['comment'].append('Name set to {} in {}{}'.format(name, device,
+                                                              partition))
+        ret['changes']['name'] = changes
+    else:
+        ret['comment'].append('Failed to set name to {}'.format(name))
+
     return ret
--- tests/unit/test_partitioned.py
+++ tests/unit/test_partitioned.py
@@ -103,7 +103,9 @@ class PartitionedTestCase(unittest.TestCase):
 
         __grains__.__getitem__.return_value = ['sda', 'sdb', 'sr0']
         __salt__.__getitem__.return_value = lambda d: {
-            'sda': hd, 'sdb': usb, 'sr0': cdrom}[d]
+            'sda': hd, 'sdb': usb, 'sr0': cdrom
+        }[d]
+
         self.assertEqual(
             partitioned.devices('only ata', {'e.id_bus': 'ata'}, {}),
             {
@@ -144,6 +146,343 @@ class PartitionedTestCase(unittest.TestCase):
                 }
             }
         )
+
+    @patch('states.partitioned.__salt__')
+    def test_get_partition_type(self, __salt__):
+        __salt__.__getitem__.return_value = lambda _: """
+Model: ATA ST2000DM001-9YN1 (scsi)
+Disk /dev/sda: 2000GB
+Sector size (logical/physical): 512B/4096B
+Partition Table: msdos
+Disk Flags:
+
+Number  Start   End     Size    Type     File system     Flags
+ 1      1049kB  2155MB  2154MB  primary  linux-swap(v1)  type=82
+ 2      2155MB  45.1GB  43.0GB  primary  btrfs           boot, type=83
+ 3      45.1GB  2000GB  1955GB  primary  xfs             type=83
+        """
+        self.assertEqual(
+            partitioned._get_partition_type('/dev/sda'),
+            [('1', 'primary'), ('2', 'primary'), ('3', 'primary')]
+        )
+
+        __salt__.__getitem__.return_value = lambda _: """
+Model: ATA QEMU HARDDISK (scsi)
+Disk /dev/sda: 25.8GB
+Sector size (logical/physical): 512B/512B
+Partition Table: msdos
+Disk Flags:
+
+Number  Start   End     Size    Type      File system  Flags
+ 1      1049kB  11.5MB  10.5MB  extended               type=05
+ 5      2097kB  5243kB  3146kB  logical                type=83
+ 3      11.5GB  22.0MB  10.5MB  primary                type=83
+        """
+        self.assertEqual(
+            partitioned._get_partition_type('/dev/sda'),
+            [('1', 'extended'), ('5', 'logical'), ('3', 'primary')]
+        )
+
+    @patch('states.partitioned.__salt__')
+    def test_get_cached_partitions(self, __salt__):
+        __salt__.__getitem__.side_effect = [
+            lambda _: '1 primary',
+            lambda _, unit: {
+                'info': None,
+                'partitions': {'1': {}},
+            },
+        ]
+
+        self.assertEqual(
+            partitioned._get_cached_partitions('/dev/sda', 's'),
+            {'1': {'type': 'primary'}}
+        )
+
+    def test_parse_value_with_units(self):
+        self.assertEqual(partitioned._parse_value_with_units(1), ('1', 'MB'))
+        self.assertEqual(partitioned._parse_value_with_units('1'), ('1', 'MB'))
+        self.assertEqual(partitioned._parse_value_with_units('1.0'),
+                         ('1.0', 'MB'))
+        self.assertEqual(partitioned._parse_value_with_units('1s'), ('1', 's'))
+        self.assertEqual(partitioned._parse_value_with_units('s1'), None)
+
+    @patch('states.partitioned._get_cached_partitions')
+    def test_check_partition(self, _get_cached_partitions):
+        _get_cached_partitions.return_value = {
+            '1': {
+                'type': 'primary',
+                'size': '10s',
+                'start': '0s',
+                'end': '10s',
+            }
+        }
+        self.assertTrue(partitioned._check_partition('/dev/sda', 1, 'primary',
+                                                     '0s', '10s'))
+        self.assertTrue(partitioned._check_partition('/dev/sda', '1',
+                                                     'primary', '0s', '10s'))
+        self.assertFalse(partitioned._check_partition('/dev/sda', '1',
+                                                      'primary', '10s', '20s'))
+        self.assertEqual(partitioned._check_partition('/dev/sda', '2',
+                                                      'primary', '10s', '20s'),
+                         None)
+
+    @patch('states.partitioned._get_cached_partitions')
+    def test_get_first_overlapping_partition(self, _get_cached_partitions):
+        _get_cached_partitions.return_value = {}
+        self.assertEqual(
+            partitioned._get_first_overlapping_partition('/dev/sda', '0s'),
+            None)
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'size': '10s',
+                'start': '0s',
+                'end': '10s',
+            }
+        }
+        self.assertEqual(
+            partitioned._get_first_overlapping_partition('/dev/sda', '0s'),
+            '1')
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'extended',
+                'size': '10s',
+                'start': '0s',
+                'end': '10s',
+            },
+            '5': {
+                'number': '5',
+                'type': 'logical',
+                'size': '4s',
+                'start': '1s',
+                'end': '5s',
+            }
+        }
+        self.assertEqual(
+            partitioned._get_first_overlapping_partition('/dev/sda', '0s'),
+            '1')
+
+        self.assertEqual(
+            partitioned._get_first_overlapping_partition('/dev/sda', '1s'),
+            '5')
+
+    @patch('states.partitioned._get_cached_info')
+    @patch('states.partitioned._get_cached_partitions')
+    def test_get_partition_number_primary(self, _get_cached_partitions,
+                                          _get_cached_info):
+        _get_cached_info.return_value = {'partition table': 'msdos'}
+        _get_cached_partitions.return_value = {}
+
+        partition_data = ('/dev/sda', 'primary', '0s', '10s')
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '1')
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'size': '10s',
+                'start': '0s',
+                'end': '10s',
+            }
+        }
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '1')
+
+        partition_data = ('/dev/sda', 'primary', '0s', '10s')
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '1')
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'start': '0s',
+                'end': '10s',
+            },
+            '2': {
+                'number': '2',
+                'type': 'primary',
+                'start': '11s',
+                'end': '20s',
+            },
+            '3': {
+                'number': '3',
+                'type': 'primary',
+                'start': '21s',
+                'end': '30s',
+            },
+            '4': {
+                'number': '4',
+                'type': 'primary',
+                'start': '31s',
+                'end': '40s',
+            },
+        }
+
+        partition_data = ('/dev/sda', 'primary', '41s', '50s')
+        self.assertRaises(partitioned.EnumerateException,
+                          partitioned._get_partition_number,
+                          *partition_data)
+
+        _get_cached_info.return_value = {'partition table': 'gpt'}
+        partition_data = ('/dev/sda', 'primary', '41s', '50s')
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '5')
+
+    @patch('states.partitioned._get_cached_info')
+    @patch('states.partitioned._get_cached_partitions')
+    def test_get_partition_number_extended(self, _get_cached_partitions,
+                                           _get_cached_info):
+        _get_cached_info.return_value = {'partition table': 'msdos'}
+        _get_cached_partitions.return_value = {}
+        partition_data = ('/dev/sda', 'extended', '0s', '10s')
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '1')
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'start': '0s',
+                'end': '10s',
+            },
+        }
+        partition_data = ('/dev/sda', 'extended', '21s', '30s')
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '2')
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'start': '0s',
+                'end': '10s',
+            },
+            '2': {
+                'number': '2',
+                'type': 'extended',
+                'start': '11s',
+                'end': '20s',
+            },
+        }
+        self.assertRaises(partitioned.EnumerateException,
+                          partitioned._get_partition_number,
+                          *partition_data)
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'start': '0s',
+                'end': '10s',
+            },
+            '2': {
+                'number': '2',
+                'type': 'primary',
+                'start': '11s',
+                'end': '20s',
+            },
+            '3': {
+                'number': '3',
+                'type': 'primary',
+                'start': '21s',
+                'end': '30s',
+            },
+            '4': {
+                'number': '4',
+                'type': 'primary',
+                'start': '31s',
+                'end': '40s',
+            },
+        }
+        partition_data = ('/dev/sda', 'extended', '41s', '50s')
+        self.assertRaises(partitioned.EnumerateException,
+                          partitioned._get_partition_number,
+                          *partition_data)
+
+        _get_cached_info.return_value = {'partition table': 'gpt'}
+        _get_cached_partitions.return_value = {}
+        self.assertRaises(partitioned.EnumerateException,
+                          partitioned._get_partition_number,
+                          *partition_data)
+
+    @patch('states.partitioned._get_cached_info')
+    @patch('states.partitioned._get_cached_partitions')
+    def test_get_partition_number_logial(self, _get_cached_partitions,
+                                         _get_cached_info):
+        _get_cached_info.return_value = {'partition table': 'msdos'}
+        _get_cached_partitions.return_value = {}
+        partition_data = ('/dev/sda', 'logical', '0s', '10s')
+        self.assertRaises(partitioned.EnumerateException,
+                          partitioned._get_partition_number,
+                          *partition_data)
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'start': '0s',
+                'end': '10s',
+            },
+        }
+        partition_data = ('/dev/sda', 'logical', '12s', '15s')
+        self.assertRaises(partitioned.EnumerateException,
+                          partitioned._get_partition_number,
+                          *partition_data)
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'start': '0s',
+                'end': '10s',
+            },
+            '2': {
+                'number': '2',
+                'type': 'extended',
+                'start': '11s',
+                'end': '20s',
+            },
+        }
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '5')
+
+        _get_cached_partitions.return_value = {
+            '1': {
+                'number': '1',
+                'type': 'primary',
+                'start': '0s',
+                'end': '10s',
+            },
+            '2': {
+                'number': '2',
+                'type': 'extended',
+                'start': '11s',
+                'end': '20s',
+            },
+            '5': {
+                'number': '5',
+                'type': 'logical',
+                'start': '12s',
+                'end': '15s',
+            },
+        }
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '5')
+
+        partition_data = ('/dev/sda', 'logical', '16s', '19s')
+        self.assertEqual(
+            partitioned._get_partition_number(*partition_data), '6')
+
+    @patch('states.partitioned._get_partition_number')
+    @patch('states.partitioned.__salt__')
+    def test_mkparted(self, __salt__, _get_partition_number):
+        pass
 
 
 if __name__ == '__main__':
