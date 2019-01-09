0e07bea0d085bd3df772081ecbde4b0e5e61e9bc
Alberto Planas <aplanas@gmail.com>
2019-01-09 16:19:33 +0100

    efi: detect secure boot configurations

--- salt/_grains/efi.py
+++ salt/_grains/efi.py
@@ -15,13 +15,24 @@
 # You should have received a copy of the GNU General Public License
 # along with Salt-autoinstaller.  If not, see <https://www.gnu.org/licenses/>.
 
+import glob
 import os.path
+
+
+def __secure_boot():
+    '''Detect if secure-boot is enabled.'''
+    enabled = False
+    sboot = glob.glob('/sys/firmware/efi/vars/SecureBoot-*/data')
+    if len(sboot) == 1:
+        enabled = open(sboot[0], 'rb').read()[-1:] == b'\x01'
+    return enabled
 
 
 def uefi():
     '''Populate UEFI grains.'''
     grains = {
-        'efi': os.path.exists('/sys/firmware/efi/systab')
+        'efi': os.path.exists('/sys/firmware/efi/systab'),
+        'efi-secure-boot': __secure_boot(),
     }
 
     return grains
