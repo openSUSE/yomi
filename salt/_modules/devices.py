86af2cb255df391f8677e5cf2accf44f3dd87f4b
Alberto Planas <aplanas@gmail.com>
2019-08-15 13:27:17 +0200

    devices.hwinfo: parse the hwinfo report

--- salt/_modules/devices.py
+++ salt/_modules/devices.py
@@ -169,6 +169,128 @@ def wipe(device):
     return True
 
 
+def _hwinfo_parse_short(report):
+    '''Parse the output of hwinfo and return a dictionary'''
+    result = {}
+    current_result = {}
+    key_counter = 0
+    for line in report.strip().splitlines():
+        if line.startswith('    '):
+            key = key_counter
+            key_counter += 1
+            current_result[key] = line.strip()
+        elif line.startswith('  '):
+            key, value = line.strip().split(' ', 1)
+            current_result[key] = value.strip()
+        elif line.endswith(':'):
+            key = line[:-1]
+            value = {}
+            result[key] = value
+            current_result = value
+            key_counter = 0
+        else:
+            LOG.error('Error parsing hwinfo short output: {}'.format(line))
+
+    return result
+
+
+def _hwinfo_parse_full(report):
+    '''Parse the output of hwinfo and return a dictionary'''
+    result = {}
+    result_stack = []
+    level = 0
+    for line in report.strip().splitlines():
+        current_level = line.count('  ')
+        if level != current_level or len(result_stack) != result_stack:
+            result_stack = result_stack[:current_level]
+            level = current_level
+        line = line.strip()
+
+        # Ignore empty lines
+        if not line:
+            continue
+
+        # Initial line of a segment
+        if level == 0:
+            key, value = line.split(':', 1)
+            sub_result = {}
+            result[key] = sub_result
+            # The first line contains also a sub-element
+            key, value = value.strip().split(': ', 1)
+            sub_result[key] = value
+            result_stack.append(sub_result)
+            level += 1
+            continue
+
+        # Line is a note
+        if line.startswith('[') or ':' not in line:
+            sub_result = result_stack[-1]
+            sub_result['Note'] = (line if not line.startswith('[')
+                                  else line[1:-1])
+            continue
+
+        key, value = line.split(':', 1)
+        key, value = key.strip(), value.strip()
+        sub_result = result_stack[-1]
+        # If there is a value and it not starts with hash, this is a
+        # (key, value) entry. But there are exception on the rule,
+        # like when is about 'El Torito info', that is the begining of
+        # a new dictorionart.
+        if value and not value.startswith('#') and key != 'El Torito info':
+            if key == 'I/O Port':
+                key = 'I/O Ports'
+            elif key == 'Config Status':
+                value = dict(item.split('=')
+                             for item in value.split(', '))
+            elif key in ('Driver', 'Driver Modules'):
+                value = value.replace('"', '').split(', ')
+            elif key in ('Tags', 'Device Files', 'Features'):
+                # We cannot split by ', ', as using spaces in
+                # inconsisten in some fields
+                value = [v.strip() for v in value.split(',')]
+            else:
+                if value.startswith('"'):
+                    value = value[1:-1]
+
+            # If there is a collision, we store it as a list
+            if key in sub_result:
+                current_value = sub_result[key]
+                if type(current_value) is not list:
+                    current_value = [current_value]
+                if value not in current_value:
+                    current_value.append(value)
+                if len(current_value) == 1:
+                    value = current_value[0]
+                else:
+                    value = current_value
+            sub_result[key] = value
+        else:
+            if value.startswith('#'):
+                value = {'Handle': value}
+            elif key == 'El Torito info':
+                value = value.split(', ')
+                value = {
+                    'platform': value[0].split()[-1],
+                    'bootable': 'no' if 'not' in value[1] else 'yes',
+                }
+            else:
+                value = {}
+
+            sub_result[key] = value
+            result_stack.append(value)
+            level += 1
+
+    return result
+
+
+def _hwinfo_parse(report, short):
+    '''Parse the output of hwinfo and return a dictionary'''
+    if short:
+        return _hwinfo_parse_short(report)
+    else:
+        return _hwinfo_parse_full(report)
+
+
 def _hwinfo_efi():
     '''Return information about EFI'''
     return {
@@ -249,7 +371,8 @@ def hwinfo(items=None, short=True, listmd=False, devices=None):
     for device in devices:
         cmd.append('--only {}'.format(device))
 
-    result['hwinfo'] = __salt__['cmd.run_stdout'](cmd)
+    out = __salt__['cmd.run_stdout'](cmd)
+    result['hwinfo'] = _hwinfo_parse(out, short)
 
     if 'bios' in items:
         result['bios grains'] = _hwinfo_efi()
