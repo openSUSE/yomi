c49de911c87e8ced585e22407ce4e12fca891446
Alberto Planas <aplanas@gmail.com>
2019-10-16 14:59:41 +0200

    suseconnect: add SUSEConnect execution module

---
+++ salt/_modules/suseconnect.py
@@ -0,0 +1,282 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Alberto Planas <aplanas@suse.com>
+#
+# Copyright 2019 SUSE LINUX GmbH, Nuernberg, Germany.
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+'''
+:maintainer:    Alberto Planas <aplanas@suse.com>
+:maturity:      new
+:depends:       None
+:platform:      Linux
+'''
+from __future__ import absolute_import, print_function, unicode_literals
+import json
+import logging
+import re
+
+import salt.utils.path
+from salt.exceptions import CommandExecutionError
+
+LOG = logging.getLogger(__name__)
+
+__virtualname__ = 'suseconnect'
+
+
+def __virtual__():
+    '''
+    Only load the module if SUSEConnect is installed
+    '''
+    if not salt.utils.path.which('SUSEConnect'):
+        return (False, 'SUSEConnect is not installed.')
+    return __virtualname__
+
+
+# Define not exported variables from Salt, so this can be imported as
+# a normal module
+try:
+    __salt__
+except NameError:
+    __salt__ = {}
+
+
+def _cmd(cmd):
+    '''Utility function to run commands.'''
+    result = __salt__['cmd.run_all'](cmd)
+    if result['retcode']:
+        raise CommandExecutionError(result['stdout'] + result['stderr'])
+    return result['stdout']
+
+
+def register(regcode, product=None, email=None, url=None, root=None):
+    '''
+    .. versionadded:: TBD
+
+    Register SUSE Linux Enterprise installation with the SUSE Customer
+    Center
+
+    regcode
+       Subscription registration code for the product to be
+       registered. Relates that product to the specified subscription,
+       and enalbes software repositories for that product.
+
+    product
+       Specify a product for activation/deactivation. Only one product
+       can be processed at a time. Defaults to the base SUSE Linux
+       Enterprose product on this system.
+       Format: <name>/<version>/<architecture>
+
+    email
+       Email address for product registration
+
+    url
+       URL for the registration server (will be saved for the next
+       use) (e.g. https://scc.suse.com)
+
+    root
+       Path to the root folder, uses the same parameter for zypper
+
+    CLI Example:
+
+    .. code-block:: bash
+
+       salt '*' suseconnect.register xxxx-yyy-zzzz
+       salt '*' suseconnect.register xxxx-yyy-zzzz product='sle-ha/15.2/x86_64'
+
+    '''
+    cmd = ['SUSEConnect', '--regcode', regcode]
+
+    parameters = [('product', product), ('email', email),
+                  ('url', url), ('root', root)]
+
+    for parameter, value in parameters:
+        if value:
+            cmd.extend(['--{}'.format(parameter), str(value)])
+
+    return _cmd(cmd)
+
+
+def deregister(product=None, url=None, root=None):
+    '''
+    .. versionadded:: TBD
+
+    De-register the system and base product, or in cojuntion with
+    'product', a single extension, and removes all its services
+    installed by SUSEConnect. After de-registration the system no
+    longer consumes a subscription slot in SCC.
+
+    product
+       Specify a product for activation/deactivation. Only one product
+       can be processed at a time. Defaults to the base SUSE Linux
+       Enterprose product on this system.
+       Format: <name>/<version>/<architecture>
+
+    url
+       URL for the registration server (will be saved for the next
+       use) (e.g. https://scc.suse.com)
+
+    root
+       Path to the root folder, uses the same parameter for zypper
+
+    CLI Example:
+
+    .. code-block:: bash
+
+       salt '*' suseconnect.deregister
+       salt '*' suseconnect.deregister product='sle-ha/15.2/x86_64'
+
+    '''
+    cmd = ['SUSEConnect', '--de-register']
+
+    parameters = [('product', product), ('url', url), ('root', root)]
+
+    for parameter, value in parameters:
+        if value:
+            cmd.extend(['--{}'.format(parameter), str(value)])
+
+    return _cmd(cmd)
+
+
+def status(root=None):
+    '''
+    .. versionadded:: TBD
+
+    Get current system registation status.
+
+    root
+       Path to the root folder, uses the same parameter for zypper
+
+    CLI Example:
+
+    .. code-block:: bash
+
+       salt '*' suseconnect.status
+
+    '''
+    cmd = ['SUSEConnect', '--status']
+
+    parameters = [('root', root)]
+
+    for parameter, value in parameters:
+        if value:
+            cmd.extend(['--{}'.format(parameter), str(value)])
+
+    return json.loads(_cmd(cmd))
+
+
+def _parse_list_extensions(output):
+    '''Parse the output of list-extensions result'''
+    # We can extract the indentation using this regex:
+    #   r'( {4,}).*\s([-\w]+/[-\w\.]+/[-\w]+).*'
+    return re.findall(r'\s([-\w]+/[-\w\.]+/[-\w]+)', output)
+
+
+def list_extensions(url=None, root=None):
+    '''
+    .. versionadded:: TBD
+
+    List all extensions and modules avaiable for installation on this
+    system.
+
+    url
+       URL for the registration server (will be saved for the next
+       use) (e.g. https://scc.suse.com)
+
+    root
+       Path to the root folder, uses the same parameter for zypper
+
+    CLI Example:
+
+    .. code-block:: bash
+
+       salt '*' suseconnect.list-extensions
+       salt '*' suseconnect.list-extensions url=https://scc.suse.com
+
+    '''
+    cmd = ['SUSEConnect', '--list-extensions']
+
+    parameters = [('url', url), ('root', root)]
+
+    for parameter, value in parameters:
+        if value:
+            cmd.extend(['--{}'.format(parameter), str(value)])
+
+    # TODO(aplanas) Implement a better parser
+    return _parse_list_extensions(_cmd(cmd))
+
+
+def cleanup(root=None):
+    '''
+    .. versionadded:: TBD
+
+    Remove olf system credential and all zypper services installed by
+    SUSEConnect
+
+    root
+       Path to the root folder, uses the same parameter for zypper
+
+    CLI Example:
+
+    .. code-block:: bash
+
+       salt '*' suseconnect.cleanup
+
+    '''
+    cmd = ['SUSEConnect', '--cleanup']
+
+    parameters = [('root', root)]
+
+    for parameter, value in parameters:
+        if value:
+            cmd.extend(['--{}'.format(parameter), str(value)])
+
+    return _cmd(cmd)
+
+
+def rollback(url=None, root=None):
+    '''
+    .. versionadded:: TBD
+
+    Revert the registration state in case of a failed migration.
+
+    url
+       URL for the registration server (will be saved for the next
+       use) (e.g. https://scc.suse.com)
+
+    root
+       Path to the root folder, uses the same parameter for zypper
+
+    CLI Example:
+
+    .. code-block:: bash
+
+       salt '*' suseconnect.rollback
+       salt '*' suseconnect.rollback url=https://scc.suse.com
+
+    '''
+    cmd = ['SUSEConnect', '--rollback']
+
+    parameters = [('url', url), ('root', root)]
+
+    for parameter, value in parameters:
+        if value:
+            cmd.extend(['--{}'.format(parameter), str(value)])
+
+    return _cmd(cmd)
---
+++ tests/unit/test_suseconnect.py
@@ -0,0 +1,362 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Alberto Planas <aplanas@suse.com>
+#
+# Copyright 2019 SUSE LINUX GmbH, Nuernberg, Germany.
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+
+import os.path
+import unittest
+from unittest.mock import patch, MagicMock
+
+from salt.exceptions import CommandExecutionError
+
+from modules import suseconnect
+
+
+class SUSEConnectTestCase(unittest.TestCase):
+    '''
+    Test cases for salt.modules.suseconnect
+    '''
+
+    def test_register(self):
+        '''
+        Test suseconnect.register without parameters
+        '''
+        result = {'retcode': 0, 'stdout': 'Successfully registered system'}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.register('regcode'),
+                             'Successfully registered system')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--regcode', 'regcode']
+            )
+
+    def test_register_params(self):
+        '''
+        Test suseconnect.register with parameters
+        '''
+        result = {'retcode': 0, 'stdout': 'Successfully registered system'}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(
+                suseconnect.register('regcode',
+                                     product='sle-ha/15.2/x86_64',
+                                     email='user@example.com',
+                                     url='https://scc.suse.com',
+                                     root='/mnt'),
+                'Successfully registered system')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--regcode', 'regcode',
+                 '--product', 'sle-ha/15.2/x86_64',
+                 '--email', 'user@example.com',
+                 '--url', 'https://scc.suse.com',
+                 '--root', '/mnt'])
+
+    def test_register_error(self):
+        '''
+        Test suseconnect.register error
+        '''
+        result = {'retcode': 1, 'stdout': 'Unknown Registration Code',
+                  'stderr': ''}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            with self.assertRaises(CommandExecutionError):
+                suseconnect.register('regcode')
+
+    def test_deregister(self):
+        '''
+        Test suseconnect.deregister without parameters
+        '''
+        result = {'retcode': 0, 'stdout': 'Successfully deregistered system'}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.deregister(),
+                             'Successfully deregistered system')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--de-register']
+            )
+
+    def test_deregister_params(self):
+        '''
+        Test suseconnect.deregister with parameters
+        '''
+        result = {'retcode': 0, 'stdout': 'Successfully deregistered system'}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(
+                suseconnect.deregister(product='sle-ha/15.2/x86_64',
+                                       url='https://scc.suse.com',
+                                       root='/mnt'),
+                'Successfully deregistered system')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--de-register',
+                 '--product', 'sle-ha/15.2/x86_64',
+                 '--url', 'https://scc.suse.com',
+                 '--root', '/mnt'])
+
+    def test_deregister_error(self):
+        '''
+        Test suseconnect.deregister error
+        '''
+        result = {'retcode': 1, 'stdout': 'Unknown Product', 'stderr': ''}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            with self.assertRaises(CommandExecutionError):
+                suseconnect.deregister()
+
+    def test_status(self):
+        '''
+        Test suseconnect.status without parameters
+        '''
+        result = {
+            'retcode': 0,
+            'stdout': '[{"identifier":"SLES","version":"15.2",'
+                      '"arch":"x86_64","status":"No Registered"}]',
+        }
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.status(),
+                             [{
+                                 'identifier': 'SLES',
+                                 'version': '15.2',
+                                 'arch': 'x86_64',
+                                 'status': 'No Registered',
+                             }])
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--status']
+            )
+
+    def test_status_params(self):
+        '''
+        Test suseconnect.status with parameters
+        '''
+        result = {
+            'retcode': 0,
+            'stdout': '[{"identifier":"SLES","version":"15.2",'
+                      '"arch":"x86_64","status":"No Registered"}]',
+        }
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.status(root='/mnt'),
+                             [{
+                                 'identifier': 'SLES',
+                                 'version': '15.2',
+                                 'arch': 'x86_64',
+                                 'status': 'No Registered',
+                             }])
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--status',
+                 '--root', '/mnt'])
+
+    def test_status_error(self):
+        '''
+        Test suseconnect.status error
+        '''
+        result = {'retcode': 1, 'stdout': 'Some Error', 'stderr': ''}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            with self.assertRaises(CommandExecutionError):
+                suseconnect.status()
+
+    def test__parse_list_extensions(self):
+        '''
+        Test suseconnect.status error
+        '''
+        fixture = os.path.join(os.path.dirname(__file__),
+                               'fixtures/list_extensions.txt')
+        with open(fixture) as f:
+            self.assertEqual(suseconnect._parse_list_extensions(f.read()), [
+                'sle-module-basesystem/15.2/x86_64',
+                'sle-module-containers/15.2/x86_64',
+                'sle-module-desktop-applications/15.2/x86_64',
+                'sle-module-development-tools/15.2/x86_64',
+                'sle-we/15.2/x86_64',
+                'sle-module-python2/15.2/x86_64',
+                'sle-module-live-patching/15.2/x86_64',
+                'PackageHub/15.2/x86_64',
+                'sle-module-server-applications/15.2/x86_64',
+                'sle-module-legacy/15.2/x86_64',
+                'sle-module-public-cloud/15.2/x86_64',
+                'sle-ha/15.2/x86_64',
+                'sle-module-web-scripting/15.2/x86_64',
+                'sle-module-transactional-server/15.2/x86_64',
+            ])
+
+    def test_list_extensions(self):
+        '''
+        Test suseconnect.list_extensions without parameters
+        '''
+        result = {
+            'retcode': 0,
+            'stdout': 'Activate with: SUSEConnect -p sle-ha/15.2/x86_64',
+        }
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.list_extensions(),
+                             ['sle-ha/15.2/x86_64'])
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--list-extensions']
+            )
+
+    def test_list_extensions_params(self):
+        '''
+        Test suseconnect.list_extensions with parameters
+        '''
+        result = {
+            'retcode': 0,
+            'stdout': 'Activate with: SUSEConnect -p sle-ha/15.2/x86_64',
+        }
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.list_extensions(
+                url='https://scc.suse.com',
+                root='/mnt'), ['sle-ha/15.2/x86_64'])
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--list-extensions',
+                 '--url', 'https://scc.suse.com',
+                 '--root', '/mnt'])
+
+    def test_list_extensions_error(self):
+        '''
+        Test suseconnect.list_extensions error
+        '''
+        result = {'retcode': 1,
+                  'stdout': 'To list extensions, you must first register '
+                            'the base product',
+                  'stderr': ''}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            with self.assertRaises(CommandExecutionError):
+                suseconnect.list_extensions()
+
+    def test_cleanup(self):
+        '''
+        Test suseconnect.cleanup without parameters
+        '''
+        result = {'retcode': 0, 'stdout': 'Service has been removed'}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.cleanup(),
+                             'Service has been removed')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--cleanup']
+            )
+
+    def test_cleanup_params(self):
+        '''
+        Test suseconnect.cleanup with parameters
+        '''
+        result = {'retcode': 0, 'stdout': 'Service has been removed'}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.cleanup(
+                root='/mnt'), 'Service has been removed')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--cleanup',
+                 '--root', '/mnt'])
+
+    def test_cleanup_error(self):
+        '''
+        Test suseconnect.cleanup error
+        '''
+        result = {'retcode': 1, 'stdout': 'some error', 'stderr': ''}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            with self.assertRaises(CommandExecutionError):
+                suseconnect.cleanup()
+
+    def test_rollback(self):
+        '''
+        Test suseconnect.rollback without parameters
+        '''
+        result = {
+            'retcode': 0,
+            'stdout': 'Starting to sync system product activations',
+        }
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.rollback(),
+                             'Starting to sync system product activations')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--rollback']
+            )
+
+    def test_rollback_params(self):
+        '''
+        Test suseconnect.rollback with parameters
+        '''
+        result = {
+            'retcode': 0,
+            'stdout': 'Starting to sync system product activations',
+        }
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            self.assertEqual(suseconnect.rollback(
+                url='https://scc.suse.com',
+                root='/mnt'), 'Starting to sync system product activations')
+            salt_mock['cmd.run_all'].assert_called_with(
+                ['SUSEConnect', '--rollback',
+                 '--url', 'https://scc.suse.com',
+                 '--root', '/mnt'])
+
+    def test_rollback_error(self):
+        '''
+        Test suseconnect.rollback error
+        '''
+        result = {'retcode': 1, 'stdout': 'some error', 'stderr': ''}
+        salt_mock = {
+            'cmd.run_all': MagicMock(return_value=result),
+        }
+        with patch.dict(suseconnect.__salt__, salt_mock):
+            with self.assertRaises(CommandExecutionError):
+                suseconnect.rollback()
